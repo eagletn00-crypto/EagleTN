39a9173af94e53887763a1e89cac2aa4e3be130f
eagletn00-crypto <eagletn00@gmail.com>
2026-06-20 17:58:51 +0100

    fix: make routing case-insensitive for mobile keyboards

--- src/App.tsx
+++ src/App.tsx
@@ -11,14 +11,30 @@ const App: React.FC = () => {
   return (
     <BrowserRouter>
       <Routes>
+        {/* التوجيه الافتراضي */}
         <Route path="/" element={<Navigate to="/client" replace />} />
+
+        {/* مسار الزبائن + حماية ضد الحروف الكبيرة */}
         <Route path="/client/*" element={<ClientRoutes />} />
-        <Route path="/livreur/*" element={<LivreurRoutes />} />
+        <Route path="/Client/*" element={<Navigate to="/client" replace />} />
+
+        {/* مسار الشركاء + حماية ضد الحروف الكبيرة */}
         <Route path="/partner/*" element={<PartnerRoutes />} />
+        <Route path="/Partner/*" element={<Navigate to="/partner" replace />} />
+
+        {/* مسار السائقين + حماية ضد الحروف الكبيرة */}
+        <Route path="/livreur/*" element={<LivreurRoutes />} />
+        <Route path="/Livreur/*" element={<Navigate to="/livreur" replace />} />
+
+        {/* مسار الإدارة + حماية ضد الحروف الكبيرة */}
         <Route path="/admin/*" element={<AdminRoutes />} />
+        <Route path="/Admin/*" element={<Navigate to="/admin" replace />} />
+
+        {/* مسار الأخطاء الحقيقي */}
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
 };
+
 export default App;
